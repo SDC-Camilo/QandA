e05ae4b12ee93d060a2b9c78cc4051a3b157d9ae
Daniel-Ghaly <ghalydan@gmail.com>
2022-01-12 18:21:19 -0800

    refactor ETL

--- ETL.sql
+++ ETL.sql
@@ -1,3 +1,16 @@
+ CREATE TABLE questions (id INT PRIMARY KEY,product_id INT,body varchar,date_written varchar,asker_name varchar,asker_email varchar,reported boolean,helpful int
+ );
+
+CREATE TABLE answers (id int PRIMARY KEY,question_id int, FOREIGN KEY(question_id) REFERENCES questions(id), body varchar,date_written varchar,answerer_name varchar,answerer_email varchar,reported boolean,helpful int);
+
+
+CREATE TABLE photos (
+id INT PRIMARY KEY,
+answer_id,
+FOREIGN KEY(answer_id) REFERENCES answers(id),
+url varchar);
+
+
 copy questions from '/Users/danielghaly/Downloads/questions.csv' with (FORMAT CSV);
 
 copy answers from '/Users/danielghaly/Downloads/answers.csv' with (FORMAT CSV);
