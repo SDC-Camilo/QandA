872cf415470e9cbfe6c57795e8e02f7a6da4f6a8
Daniel-Ghaly <ghalydan@gmail.com>
2022-01-12 18:23:38 -0800

    clean up ETL readability

--- ETL.sql
+++ ETL.sql
@@ -1,8 +1,22 @@
- CREATE TABLE questions (id INT PRIMARY KEY,product_id INT,body varchar,date_written varchar,asker_name varchar,asker_email varchar,reported boolean,helpful int
+ CREATE TABLE questions (id INT PRIMARY KEY,
+ product_id INT,
+ body varchar,
+ date_written varchar,
+ asker_name varchar,
+ asker_email varchar,
+ reported boolean,
+ helpful int
  );
 
-CREATE TABLE answers (id int PRIMARY KEY,question_id int, FOREIGN KEY(question_id) REFERENCES questions(id), body varchar,date_written varchar,answerer_name varchar,answerer_email varchar,reported boolean,helpful int);
-
+CREATE TABLE answers (id int PRIMARY KEY,
+question_id int,
+FOREIGN KEY(question_id) REFERENCES questions(id),
+body varchar,
+date_written varchar,
+answerer_name varchar,
+answerer_email varchar,
+reported boolean,
+helpful int);
 
 CREATE TABLE photos (
 id INT PRIMARY KEY,
@@ -10,9 +24,6 @@ answer_id,
 FOREIGN KEY(answer_id) REFERENCES answers(id),
 url varchar);
 
-
 copy questions from '/Users/danielghaly/Downloads/questions.csv' with (FORMAT CSV);
-
 copy answers from '/Users/danielghaly/Downloads/answers.csv' with (FORMAT CSV);
-
 copy photos from '/Users/danielghaly/Downloads/answers_photos.csv' with (FORMAT CSV);
