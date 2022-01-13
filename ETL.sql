f17bba72d7396091a91821d7e67eb0a18e2be81c
Daniel-Ghaly <ghalydan@gmail.com>
2022-01-12 18:15:28 -0800

    database creation and ETL

---
+++ ETL.sql
@@ -0,0 +1,5 @@
+copy questions from '/Users/danielghaly/Downloads/questions.csv' with (FORMAT CSV);
+
+copy answers from '/Users/danielghaly/Downloads/answers.csv' with (FORMAT CSV);
+
+copy photos from '/Users/danielghaly/Downloads/answers_photos.csv' with (FORMAT CSV);
